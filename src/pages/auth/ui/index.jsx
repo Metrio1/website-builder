54beab7a4ec8a2297533c31d3f5b00659e2c8cbf
Dmitriy_Zakirov <metrio@inbox.ru>
2024-02-26 16:18:19 +0500

    Adding a Formik hook

--- src/pages/auth/ui/index.jsx
+++ src/pages/auth/ui/index.jsx
@@ -1,5 +1,6 @@
 import './index.scss';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 export default function Auth() {
   const handleClick = () => console.log('Авторизация прошла успешно');
@@ -10,18 +11,41 @@ export default function Auth() {
       <div className="authorization">
         <h1 className="authorization__header">Авторизация</h1>
         <div className="authorization__content">
-          <div className="authorization__fields">
-            <label htmlFor="surname">Логин</label>
-            <input type="text"></input>
-            <label htmlFor="password">Пароль</label>
-            <input type="text"></input>
-            <button onClick={handleClick} type="submit" className="auth-btn">
-              Авторизоваться
-            </button>
-            <Link to="/registration" relative="path">
-              Ещё нет аккаунта?
-            </Link>
-          </div>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = 'Required';
+              } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+              }
+              if (!values.password) {
+                errors.password = 'Required';
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                console.log(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form className="form-hook">
+                <label htmlFor="email">Email</label>
+                <Field type="email" name="email" />
+                <ErrorMessage name="email" component="div" />
+                <label htmlFor="password">Password</label>
+                <Field type="password" name="password" />
+                <ErrorMessage name="password" component="div" />
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </main>
--- src/pages/reg/ui/index.jsx
+++ src/pages/reg/ui/index.jsx
@@ -1,5 +1,7 @@
 import './index.scss';
 import { Link } from 'react-router-dom';
+import { Formik, Form, ErrorMessage, Field } from 'formik';
+import * as Yup from 'yup';
 
 export default function Reg() {
   const handleClick = () => console.log('Регистрация прошла успешно');
@@ -10,26 +12,72 @@ export default function Reg() {
       <div className="registration">
         <h1 className="registration__header">Регистрация</h1>
         <div className="registration__content">
-          <div className="registration__fields">
-            <label htmlFor="name">Имя</label>
-            <input type="text"></input>
-            <label htmlFor="surname">Фамилия</label>
-            <input type="text"></input>
-            <label htmlFor="surname">Логин</label>
-            <input type="text"></input>
-            <label htmlFor="email">Email</label>
-            <input type="text"></input>
-            <label htmlFor="password">Пароль</label>
-            <input type="text"></input>
-            <label htmlFor="password-repeat">Повторение пароля</label>
-            <input type="password"></input>
-            <button onClick={handleClick} type="submit" className="reg-btn">
-              Зарегистрироваться
-            </button>
-            <Link to="/auth" relative="path" className="link-to-auth">
-              Уже есть аккаунт?
-            </Link>
-          </div>
+          <Formik
+            initialValues={{
+              first_name: '',
+              surname: '',
+              login: '',
+              email: '',
+              password: '',
+              password_repeat: '',
+            }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = 'Required';
+              } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+              }
+              if (!values.first_name) {
+                errors.first_name = 'Required';
+              }
+              if (!values.surname) {
+                errors.surname = 'Required';
+              }
+              if (!values.login) {
+                errors.login = 'Required';
+              }
+              if (!values.password) {
+                errors.password = 'Required';
+              }
+              if (values.password !== values.password_repeat) {
+                errors.password_repeat = 'Пароли не совпадают';
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                console.log(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form className="form-hook">
+                <label htmlFor="first_name">First name</label>
+                <Field type="first_name" name="first_name" />
+                <ErrorMessage name="first_name" component="div" />
+                <label htmlFor="surname">Surname</label>
+                <Field type="surname" name="surname" />
+                <ErrorMessage name="surname" component="div" />
+                <label htmlFor="login">Login</label>
+                <Field type="login" name="login" />
+                <ErrorMessage name="login" component="div" />
+                <label htmlFor="email">Email</label>
+                <Field type="email" name="email" />
+                <ErrorMessage name="email" component="div" />
+                <label htmlFor="password">Password</label>
+                <Field type="password" name="password" />
+                <ErrorMessage name="password" component="div" />
+                <label htmlFor="password_repeat">Repeat password</label>
+                <Field type="password" name="password_repeat" />
+                <ErrorMessage name="password_repeat" component="div" />
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </main>
