418f74deda42fe2bef8fbb799996562cbb124c14
Dmitriy_Zakirov <metrio@inbox.ru>
2024-03-31 12:03:10 +0500

    Template selection done

--- src/pages/basic/ui/index.jsx
+++ src/pages/basic/ui/index.jsx
@@ -1,15 +1,7 @@
 import './index.scss';
-import templateSlice, { setTemplateId } from '../../../entities/template/model/template.slice.js';
-import { store } from '../../../app/appStore.js';
+import TemplateSelection from '../../../widgets/template-selection/ui/index.jsx';
 
 export default function Basic() {
-  const handleSelection = (layoutType) => {
-    console.log(store.getState());
-    store.dispatch(setTemplateId(layoutType));
-    console.log(store.getState());
-    // console.log(initialState);
-  };
-
   return (
     <div className="basic">
       <header>
@@ -21,33 +13,21 @@ export default function Basic() {
         <div>
           <h1 className="basic__title">Выбор базового шаблона</h1>
           <div className="basic__templates">
-            <div className="basic__template">
-              <img className="basic__layout" src="first_layout.svg" width="480" height="270" />
-              <div className="basic__description">
-                <h3>Шаблон с левой боковой панелью</h3>
-                <button className="basic__button" onClick={() => handleSelection('first_layout')}>
-                  Выбрать
-                </button>
-              </div>
-            </div>
-            <div className="basic__template">
-              <img className="basic__layout" src="second_layout.svg" width="480" height="270" />
-              <div className="basic__description">
-                <h3>Шаблон без боковой панели</h3>
-                <button className="basic__button" onClick={() => handleSelection('second_layout')}>
-                  Выбрать
-                </button>
-              </div>
-            </div>
-            <div className="basic__template">
-              <img className="basic__layout" src="third_layout.svg" width="480" height="270" />
-              <div className="basic__description">
-                <h3>Шаблон с правой боковой панелью</h3>
-                <button className="basic__button" onClick={() => handleSelection('third_layout')}>
-                  Выбрать
-                </button>
-              </div>
-            </div>
+            <TemplateSelection
+              image={'first_layout.svg'}
+              description={'Шаблон с левой боковой панелью'}
+              layoutNumber={'first_layout'}
+            />
+            <TemplateSelection
+              image={'second_layout.svg'}
+              description={'Шаблон без боковой панели'}
+              layoutNumber={'second_layout'}
+            />
+            <TemplateSelection
+              image={'third_layout.svg'}
+              description={'Шаблон с правой боковой панелью'}
+              layoutNumber={'third_layout'}
+            />
           </div>
         </div>
       </main>
--- src/widgets/template-selection/ui/index.jsx
+++ src/widgets/template-selection/ui/index.jsx
@@ -1,5 +1,24 @@
 import './index.scss';
+import { store } from '../../../app/appStore.js';
+import { setTemplateId } from '../../../entities/template/model/template.slice.js';
 
-export default function TemplateSelection() {
-  return ();
+export default function TemplateSelection({ image, description, layoutNumber }) {
+  const handleSelection = (layoutType) => {
+    console.log(store.getState());
+    store.dispatch(setTemplateId(layoutType));
+    console.log(store.getState());
+    // console.log(initialState);
+  };
+
+  return (
+    <div className="basic__template">
+      <img className="basic__layout" src={image} width="480" height="270" />
+      <div className="basic__description">
+        <h3>{description}</h3>
+        <button className="basic__button" onClick={() => handleSelection({ layoutNumber })}>
+          Выбрать
+        </button>
+      </div>
+    </div>
+  );
 }
